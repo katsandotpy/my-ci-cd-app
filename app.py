dbd5812696599dece28f90abbd42031563cd8aec
Кацан Михаил Вячеславович <s22270019@unn.ru>
2025-09-18 10:21:59 +0300

    tests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
@@ -10,4 +10,3 @@ def health():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
